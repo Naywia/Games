d86e5d85ab78e530b6a584c3f184095788c1253e
Naywia <48431873+Naywia@users.noreply.github.com>
2020-04-20 10:59:40 +0200

    ...

--- index.php
+++ index.php
@@ -11,23 +11,9 @@
         
         <link rel="stylesheet" href="/../style.css">
         <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
-        <script>
-            $(document).ready(function () {
-                $("form").submit(function () {
-                    alert("Submitted");
-                    $("p").after("<p>Succesfully submitted</p>");
-                });
-            });
-        </script>
     </head>
     <body>
         <h1>Hello There!</h1>
-        <p>This site is for a database over games 666</p><br><br>
-
-        <form action="">
-            E-mail: <input type="email" name="email" placeholder="e-mail"><br>
-            Password: <input type="password" name="password" placeholder="password"><br>
-            <input type="submit" value="login">
-        </form> 
+        <p>This site is for a database over games</p><br><br>
     </body>
 </html>
