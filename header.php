5a5858714554a4a4c455e2b70236521da150f091
Naywia <48431873+Naywia@users.noreply.github.com>
2020-04-20 15:25:57 +0200

    update

--- header.php
+++ header.php
@@ -1,10 +1,19 @@
 <?php
 
-echo '<header>
-            <div class="left">
+echo '
+  <nav class="navbar">
+        <div class="imageContainer">
+            <img class="logoImage" src="/../images/controller.png"/>
+        </div>
+        <div class="title">
+            <h1>Games Database</h1>
+        </div>
+        <div class="navLinks">
+            <div class="nav">
+                <a class="navLink" href="index.php">Hjem</a>
             </div>
-            <div class="title">
-                <h1>Games Database</h1>
+            <div class="nav">
+                <a class="navLink" href="login.html">Login</a>
             </div>
-            <div class="right"></div>
-        </header>';
+        </div>
+    </nav>';
