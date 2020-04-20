24d07a58f6ec38b5da1bb1530ef5213f7ba6ac7e
Naywia <48431873+Naywia@users.noreply.github.com>
2020-04-20 09:33:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,25 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Games</title>
+        <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+        <script>
+            $(document).ready(function () {
+                $("form").submit(function () {
+                    alert("Submitted");
+                });
+            });
+        </script>
     </head>
     <body>
-        <?php
-            echo "HELLO!";
-        ?>
+        <h1>Hello There!</h1>
+        <p>This site is for a database over games 666</p><br><br>
+
+        <form action="">
+            E-mail: <input type="email" name="email" placeholder="e-mail"><br>
+            Password: <input type="password" name="password" placeholder="password"><br>
+            <input type="submit" value="login">
+        </form> 
     </body>
 </html>
