72ca89e54010011cd1eac34236445a66c1188344
Naywia <48431873+Naywia@users.noreply.github.com>
2020-04-20 10:32:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
     <head>
         <meta charset="UTF-8">
         <title>Games</title>
+        
+         <!-- Site Icons -->
+        <link rel="apple-touch-icon" sizes="180x180" href="/../images/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/../images/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/../images/favicon/favicon-16x16.png">
+        
         <link rel="stylesheet" href="/../style.css">
         <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
         <script>
