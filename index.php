574c1c6d53aa83bd02cba18279a7edc7c95824bd
Naywia <48431873+Naywia@users.noreply.github.com>
2020-04-21 12:12:36 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
-<!DOCTYPE html>
 <?php
 // Start a session
 session_start();
 ?>
+<!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
@@ -18,9 +18,49 @@ session_start();
     </head>
     <body>
         <!-- Include header -->
-        <?php include_once 'header.php'; ?>
+        <?php
+        include_once 'header.php';
 
-        <h1>Most recent games</h1><br>
+        $sql = "
+            SELECT gdb_games.gameID, gameName, description, releaseDate, genre FROM gdb_gameGenre 
+            INNER JOIN gdb_games ON gdb_gameGenre.gameID = gdb_games.gameID 
+            INNER JOIN gdb_genres ON gdb_gameGenre.genreID = gdb_genres.genreID 
+            ORDER BY releaseDate DESC LIMIT 5 ";
+
+        $result = $conn->query($sql);
+
+        while ($row = mysqli_fetch_array($result)) {
+            $data[] = $row;
+        }
+        ?>
+
+        <section id="projects" class="projectSection">
+            <div class="container">
+                <h1>Most recent games</h1><br>
+
+                <div class="galleryRow">
+                    <?php
+                    foreach ($data as $row) {
+                        $gameID = $row['gdb_games.gameID'];
+                        $gameName = $row['gameName'];
+                        $description = $row['description'];
+                        $releaseDate = $row['releaseDate'];
+
+                        echo "
+                            <a class='' href='project.php?game=" . $gameID . "'>
+                                <div class='gameImages'>
+                                    <img class='projectPicture' src='/../images/games/" . $imageName . ".png' class='' alt='projectImage'>
+                                    <div class='projectName'> 
+                                        <h2> " . $projectName . " </h2>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>";
+                    }
+                    ?>
+                </div>
+            </div>
+        </section>
 
         <?php
         print_r($_SESSION);
