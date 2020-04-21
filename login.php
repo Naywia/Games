67e664e484c03b526f583a89e8e207fe07e6f6af
rasm947f <rasm947f@gmail.com>
2020-04-21 08:19:00 +0200

    Work on login

--- login.php
+++ login.php
@@ -1,24 +1,22 @@
 <?php
+
 include_once 'connection.php';
 
+$email = filter_input(INPUT_POST, "email");
+$password = filter_input(INPUT_POST, "password");
 
-$email = mysqli_real_escape_string($conn,$_POST['username']);
-$password = mysqli_real_escape_string($conn,$_POST['password']);
+if ($email != "" && $password != "") {
 
-if ($email != "" && $password != ""){
-
-    $sql_query = "select count(*) as cntUser from users where username='".$email."' and password='".$password."'";
-    $result = mysqli_query($conn,$sql_query);
+    $sql_query = "select count(*) as cntUser from users where username='" . $email . "' and password='" . $password . "'";
+    $result = mysqli_query($conn, $sql_query);
     $row = mysqli_fetch_array($result);
 
     $count = $row['cntUser'];
 
-    if($count > 0){
+    if ($count > 0) {
         $_SESSION['email'] = $email;
         echo 1;
-    }else{
+    } else {
         echo 0;
     }
-
 }
-
