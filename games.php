69624b3011625a670221aede3903eb18f05ee08f
Naywia <48431873+Naywia@users.noreply.github.com>
2020-04-21 12:50:51 +0200

    loads of pictures

--- games.php
+++ games.php
@@ -1,8 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Games</title>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+        <!-- Site Icons -->
+        <link rel="apple-touch-icon" sizes="180x180" href="/../images/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/../images/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/../images/favicon/favicon-16x16.png">
 
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <!-- Include header -->
+        <?php include_once 'header.php';?>
+        
+        <h1>All games!</h1>
+    </body>
+</html>
