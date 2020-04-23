76163e3eb473f81a2267000f07d73a1ba2b532e3
rasm947f <rasm947f@gmail.com>
2020-04-23 11:16:41 +0200

    Added vue (view)

--- vue.php
+++ vue.php
@@ -8,7 +8,7 @@ and open the template in the editor.
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta charset="UTF-8">
-        <title>Weather API</title>
+        <title>Vue Framework</title>
 
         <!-- Site Icons -->
         <link rel="apple-touch-icon" sizes="180x180" href="/../images/favicon/apple-touch-icon.png">
@@ -21,32 +21,35 @@ and open the template in the editor.
 
         <!-- Js -->
         <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
     </head>
     <body>
-        <?php
-        include_once 'header.php';
-        ?>
-        <div class="container">
-            <div id="displayWeather">
-                <?php
-                // Initialize a curl resource. The URL can also be specified here
-                $curl = curl_init();
-
-                // Set the curl options to be the curl resource, the setting and the value
-                // CURLOPT_URL simply specifies that the URL is being changed.
-                // After the city name, mode can be added to change how the data is displayed.
-                curl_setopt($curl, CURLOPT_URL, 'https://api.openweathermap.org/data/2.5/weather?q=fredericia,dk&mode=html&appid=69187b1a3be7430370c6e6fdd5d0c496');
-
-                // Execute the curl
-                $result = curl_exec($curl);
-
-                // Close the curl object
-                curl_close($curl);
-                ?>
-            </div>
+        <div id="app">
+            {{ message }}
+        </div>
+        <div id="app-2">
+            <span v-bind:title="message">
+                Hover your mouse over me for a few seconds
+                to see my dynamically bound title!
+            </span>
         </div>
         <?php
-        include_once 'footer.php';
+        // put your code here
         ?>
+        <script>
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+})
+        </script>
     </body>
 </html>
