6d176503b64d98d4e88f8d873d636580a7b2834e
rasm947f <rasm947f@gmail.com>
2020-04-23 09:56:48 +0200

    Comments on the weather

--- weather.php
+++ weather.php
@@ -12,11 +12,17 @@ and open the template in the editor.
     </head>
     <body>
         <?php
+        // Initialize a curl resource. The URL can also be specified here
         $curl = curl_init();
 
+        // Set the curl options to be the curl resource, the setting and the value
+        // CURLOPT_URL simply specifies that the URL is being changed.
         curl_setopt($curl, CURLOPT_URL, 'https://api.openweathermap.org/data/2.5/weather?q=fredericia,dk&appid=69187b1a3be7430370c6e6fdd5d0c496');
+        
+        // Execute the curl
         $result = curl_exec($curl);
 
+        // Close the curl object
         curl_close($curl);
         ?>
     </body>
