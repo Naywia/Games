9240e1d49c31f35274ac8d7522b29b6856a33b52
Naywia <48431873+Naywia@users.noreply.github.com>
2020-04-24 08:28:51 +0200

    Probably last commit before exam

--- footer.php
+++ footer.php
@@ -1,11 +1,8 @@
 <footer id="footer">
-    <div class="footerLeft">
+    <div class="footerCenter">
         <p class="footerLinks">
             <?php echo date("Y") . " "; ?><a target="_blank" href="/../images/random/RasmusAndMaya.png">Rasmus Nørby & Maya Wettendorff</a>
             | Favicon by: <a target="_blank" href="https://favicon.io">Favicon.io</a>
         </p>
-    </div>
-    <div class="footerRight">
-        
     </div> <!-- end footerRight -->
 </footer> <!-- end footer -->
